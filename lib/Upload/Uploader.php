df62f0fc05bb7cfed2734f3672d7379ca3b60a07
Irmantas Šiupšinskas <i.siupsinskas@gmail.com>
2012-11-04 18:57:32 +0200

    Added basic setters/getters to Uploader class, and some other functions

--- lib/Upload/Uploader.php
+++ lib/Upload/Uploader.php
@@ -2,8 +2,165 @@
 
 namespace Upload;
 
+use Upload\Handler\HandlerInterface;
 
 class Uploader
 {
+    /**
+     * @var array
+     */
+    protected $_allowExtensions = array();
 
+    /**
+     * @var int
+     */
+    protected $_minSize = 0;
+
+    /**
+     * @var int
+     */
+    protected $_maxSize = 5242880;
+
+    /**
+     * @var null
+     */
+    protected $_uploadDirectory = null;
+
+    /**
+     * @var null
+     */
+    protected $_filename = null;
+
+    /**
+     * @var HandlerInterface
+     */
+    private $_handler;
+
+    const ERR_FILE_EMPTY    = 'err_file_empty';
+    const ERR_FILE_LARGE    = 'err_file_large';
+    const ERR_FILE_SMALL    = 'err_file_small';
+    const ERR_FILE_EXT      = 'err_file_ext';
+    const ERR_UPLOAD        = 'err_file_upload';
+    const ERR_UPLOAD_DIR    = 'err_file_upload_dir';
+    const ERR_WRITE_DIR    = 'err_file_write_dir';
+
+    protected $_errorMessages = array(
+        self::ERR_FILE_EMPTY => 'File is empty',
+        self::ERR_FILE_LARGE => 'File is too large',
+        self::ERR_FILE_SMALL => 'File is too small',
+        self::ERR_FILE_EXT => 'File has an invalid extension',
+        self::ERR_UPLOAD => 'Upload error',
+        self::ERR_UPLOAD_DIR => 'Upload directory is not set',
+        self::ERR_WRITE_DIR => 'Upload directory is not writable',
+    );
+
+    protected $_errors = array();
+
+    /**
+     * @param HandlerInterface $handler
+     */
+    public function __construct(HandlerInterface $handler)
+    {
+        $this->_handler = $handler;
+    }
+
+    /**
+     * @param int $size
+     * @return Uploader
+     */
+    public function setMinSize($size = 0)
+    {
+        $this->_minSize = $this->_checkAndClearSize($size);
+        return $this;
+    }
+
+    /**
+     * @param $size
+     * @return Uploader
+     */
+    public function setMaxSize($size)
+    {
+        $this->_maxSize = $this->_checkAndClearSize($size);
+        return $this;
+    }
+
+    /**
+     * @param $path
+     * @return Uploader
+     */
+    public function setUploadDir($path)
+    {
+        $this->_uploadDirectory = rtrim($path, '/') . '/';
+        return $this;
+    }
+
+    /**
+     * @param array $extensions
+     * @return Uploader
+     */
+    public function setAllowExtensions(array $extensions = array())
+    {
+        $this->_allowExtensions = $extensions;
+        return $this;
+    }
+
+    /**
+     * @param string $filename
+     * @return Uploader
+     */
+    public function setFilename($filename)
+    {
+        $this->_filename = $filename;
+        return $this;
+    }
+
+    /**
+     * @param $error
+     * @return Uploader
+     */
+    public function setError($error)
+    {
+        $this->_errors[] = $error;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasErrors()
+    {
+        return !empty($this->_errors);
+    }
+
+    public function getErrors()
+    {
+        return $this->_errors;
+    }
+
+    public function handle()
+    {
+
+    }
+
+    private function _check()
+    {
+        if (empty($this->_uploadDirectory)) {
+            $this->setError($this->_errorMessages[self::ERR_UPLOAD_DIR]);
+        }
+    }
+
+    /**
+     * @param $size
+     * @return int
+     * @throws \Exception
+     */
+    private function _checkAndClearSize($size)
+    {
+        $size = (int)$size;
+        if ($size < 0) {
+            throw new \Exception('Upload size cannot be negative value');
+        }
+
+        return $size;
+    }
 }
